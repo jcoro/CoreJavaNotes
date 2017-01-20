ea0c28705e7f77c46ce50dddfb111318a1bdd87b
jcoro <jcoronite@hotmail.com>
2017-01-19 20:00:13 -0700

    feat: Add notes and code to CollectionsSandbox

--- README.md
+++ README.md
@@ -24,6 +24,38 @@ dependencies {
 
 - Make sure to right-click the `test` folder and Mark Directory As > Test Sources Root.
 
+
+## Collections - Notes
+
+### Describe some best use cases for iterators and for loops
+
+We know that:
+- Lists (e.g., LinkedLists and ArrayLists) have indices and are ordered.
+- Sets (e.g., HashSets and TreeSets) don't have indices and are unordered.
+- the Collection.add(E e) method returns a boolean and adds an element to the end of a List
+- Adding elements in the middle of an ordered list requires an iterator.
+- The "for each" loop uses an iterator "under the hood."
+- We should never use a (plain) for loop to step through a LinkedList - each time an element is
+looked up, the search starts again from the beginning.
+
+Use a for loop:
+- On arrays and ArrayLists when you don't want to traverse the entire collection or when the index
+value is needed in the loop.
+
+Use iterators or "for each" loops.
+- When each element needs to be traversed and the index value isn't needed in the loop.
+- Use LinkedLists when elements need to be inserted and removed in the middle, and use iterators
+to facilitate this.
+
+### What is the difference between an Iterator and a ListIterator?
+
+Because (unlike LinkedLists, for example) Sets are unordered, the Iterator interface has
+no add() method (instead, we call .add() on the Collection itself - not the Iterator).
+The ListIterator<E> is a subinterface used on Lists which does have an add()
+method and which doesn't return a boolean because Lists allow multiple instances of elements which
+are equal.
+
+
 ## Useful Keyboard Shortcuts (Mac)
 
 ### Android Studio
--- src/main/java/sandbox/CollectionsSandbox.java
+++ src/main/java/sandbox/CollectionsSandbox.java
@@ -1,9 +1,13 @@
 package sandbox;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * A class used to experiment with Collections
@@ -13,7 +17,10 @@ import java.util.ListIterator;
 public class CollectionsSandbox {
 
     public static void main(String[] args){
-        studyLinkedList();
+        //studyLinkedList();
+        studyLinkedList2();
+        //studySets();
+        //studySets2("some some strings to be used to test test the method");
     }
 
     /**
@@ -58,6 +65,76 @@ public class CollectionsSandbox {
             // [Amy, Bob, Carl, Doug, Erica]
             // [Amy, Bob, Carl, Doug, Erica, Frances]
             // [Amy, Bob, Carl, Doug, Erica, Frances, Gloria]
+    }
+
+    /*
+    Instead of a loop, this method uses Iterator.forEachRemaining() method which takes a Lambda Expression
+    that consumes an element Iterator.forEachRemaining(Consumer<? super E> action)
+     */
+    public static void studyLinkedList2(){
+        List<String> team = new LinkedList<>();
+                team.add("Larry Bird");
+                team.add("Danny Ainge");
+                team.add("Rober Parish");
+                team.add("Dennis Johnson");
+                team.add("Kevin McHale");
+        Iterator<String> teamIterator = team.iterator();
+        //teamIterator.forEachRemaining(e -> System.out.println(e)); is the same as the method ref:
+        teamIterator.forEachRemaining(System.out::println);
+
+    }
+        /*
+        HashSets implements Set interface, backed by a hash table (actually a HashMap instance).
+        Lookup is faster than LinkedLists and Arrays
+        Elements cannot be repeated.
+        Elements can be stored in any order.
+         */
+
+        public static String studySets() {
+
+            Set<String> words = new HashSet<>(); // HashSet implements Set
+            long totalTime = 0;
+
+            System.out.println("Enter Words and Press CMD + D when done");
+            try (Scanner in = new Scanner(System.in))
+            {
+                while (in.hasNext()) {
+                    String word = in.next();
+                    System.out.println("word entered:" + word);
+                    long callTime = System.currentTimeMillis();
+                    words.add(word);
+                    callTime = System.currentTimeMillis() - callTime;
+                    totalTime += callTime;
+                }
+            }
+
+            Iterator<String> iter = words.iterator();
+            for (int i = 1; i <= 20 && iter.hasNext(); i++)
+                System.out.println(iter.next());
+            System.out.println(". . .");
+            String returnValue = words.size() + " distinct words." + totalTime + " milliseconds ";
+            System.out.println(returnValue);
+            return returnValue;
+
+        }
+    /*
+    This method prints each
+     */
+    public static int studySets2(String input) {
+
+        Set<String> words = new HashSet<>(); // HashSet implements Set
+        long totalTime = 0;
+
+        String[] inputArray = input.split(" ");
+
+        words.addAll(Arrays.asList(inputArray));
+
+        Iterator<String> iter = words.iterator();
+        for (String w : words)
+            System.out.println(w);
+        System.out.println(". . .");
+        System.out.println(words.size() + " unique words");
+        return words.size();
 
     }
 }
